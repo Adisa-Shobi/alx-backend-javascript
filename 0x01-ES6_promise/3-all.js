800709014095f3757d57a05ed89bf695e2f82461
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-01-26 08:38:30 +0000

    Update: '3-all.js' using promise.all to collectively fufill promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,17 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  let firstName; let lastName; let
-    body;
-  uploadPhoto()
-    .then((resolve) => {
-      body = resolve.body;
-      return createUser();
-    })
-    .then((resolve) => {
-      firstName = resolve.firstName;
-      lastName = resolve.lastName;
-      console.log(body, firstName, lastName);
-    })
-    .catch(() => { console.log('Signup system offline'); });
+  Promise.all([uploadPhoto(), createUser()]).then((values) => {
+    console.log(values[0].body, values[1].firstName, values[1].lastName);
+  }).catch(() => { console.log('Signup system offline'); });
 }
