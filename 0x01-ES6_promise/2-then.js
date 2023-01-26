93e8e13865aa8f624f56ab63812d8377f3b53dc2
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-01-26 07:39:03 +0000

    Update: '2-then.js' all handlers added

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,7 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    console.log('Got a response from the API');
-    return ({
-      status: 200,
-      body: 'success',
-    });
-  }).catch(() => (new Error()));
+  const response = { status: 200, body: 'success' };
+  return promise
+    .then(() => response)
+    .catch(() => Error())
+    .finally(() => { console.log('Got a response from the API'); });
 }
