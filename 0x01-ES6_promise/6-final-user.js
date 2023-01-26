9407b5fdbbbf6f320b4302ae85fdb5adebbed14d
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-01-26 16:55:20 +0000

    Create: '6-main.js' return info on promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,20 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const photo = uploadPhoto(fileName);
+  const user = signUpUser(firstName, lastName);
+  const response = [];
+  return Promise.allSettled([photo, user])
+    .then((data) => {
+      let status;
+      let value;
+      data.forEach((item) => {
+        status = item.status;
+        if (status === 'rejected') value = item.reason;
+        else value = item.value;
+        response.push({ status, value });
+      });
+      return response;
+    });
+}
