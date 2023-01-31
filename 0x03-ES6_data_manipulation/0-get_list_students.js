e2d57faee1f33c33887c6beeac2d06e104312afc
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-01-31 09:26:14 +0000

    Create '0-get_list_students.js' creates a new array

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,8 @@
+export default function getListStudents() {
+  const myArray = [
+    { firstName: 'Guillaume', location: 'San Francisco', id: 1 },
+    { firstName: 'James', location: 'Columbia', id: 2 },
+    { firstName: 'Serena', location: 'San Francisco', id: 5 },
+  ];
+  return myArray;
+}
---
+++ 0x03-ES6_data_manipulation/README.md
@@ -0,0 +1,2 @@
+# Files and their functions
+- 0-get_list_students.js - a function named getListStudents that returns an array of objects.
