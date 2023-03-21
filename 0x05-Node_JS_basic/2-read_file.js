118c2a544842f4581e4e8f30b418350b4324077f
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-21 07:32:55 +0000

    Update: '2-read_file.js' eslint adherent

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-function countStudents (path) {
+function countStudents(path) {
   let data;
   try {
     data = fs.readFileSync(path, 'utf8');
@@ -13,7 +13,7 @@ function countStudents (path) {
   const numberOfStudents = lines.length ? lines.length - 1 : 0;
   console.log(`Number of students: ${numberOfStudents}`);
   const fields = {};
-  for (let i = 1; i < lines.length; i++) {
+  for (let i = 1; i < lines.length; i = 1 + 1) {
     const line = lines[i];
     const field = line[line.length - 1];
     if (field in fields) { fields[field].push(line[0]); } else { fields[field] = [line[0]]; }
