e1a13f105ffe474d0e89900cfe6d8c2fad7950bd
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-21 15:05:46 +0000

    Update: '5-http.js' modified

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,8 +1,42 @@
 const http = require('http');
-const countStudents = require('./3-read_file_async');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 1245;
+
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (data === undefined || err) {
+        reject(new Error('Cannot load the database'));
+        return;
+      }
+      const resp = [];
+      let lines = data.toString().split(/\r\n|\n/);
+      lines = lines.filter((item) => item !== '');
+      lines = lines.map((item) => item.split(','));
+      const numberOfStudents = lines.length ? lines.length - 1 : 0;
+      let msg = `Number of students: ${numberOfStudents}`;
+      resp.push(msg);
+      const fields = {};
+      for (let i = 1; i < lines.length; i += 1) {
+        const line = lines[i];
+        const field = line[line.length - 1];
+        if (field in fields) {
+          fields[field].push(line[0]);
+        } else {
+          fields[field] = [line[0]];
+        }
+      }
+      for (const [key, value] of Object.entries(fields)) {
+        msg = `Number of students in ${key}: ${value.length}.\
+ List: ${value.join(', ')}`;
+        resp.push(msg);
+      }
+      resolve(resp);
+    });
+  });
+}
 
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
