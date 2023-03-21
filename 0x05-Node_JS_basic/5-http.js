3d162bee9030949a88d458756249cd6f15d1c500
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-21 08:58:53 +0000

    Create: '5-http.js' route /students responds with student info

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,29 @@
+const http = require('http');
+const url = require('url');
+const countStudents = require('./3-read_file_async');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer(async (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  const reqUrl = url.parse(req.url).pathname;
+  if (reqUrl === '/') {
+    res.write('Hello Holberton School!');
+    res.end();
+  }
+  if (reqUrl === '/students') {
+    const database = process.argv[2];
+    const data = await countStudents(database);
+    res.write('This is the list of our students\n');
+    res.write(data.join('\n'));
+    res.end();
+  }
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
