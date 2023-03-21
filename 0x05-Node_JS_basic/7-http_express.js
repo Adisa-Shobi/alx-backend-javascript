9d195ad88c37df2cc718fa5c39d892c7c7d9ddf6
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-21 16:58:37 +0000

    Update: '7-http_express.js' simple express app

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+const database = process.argv[2] ? process.argv[2] : '';
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  const pre = 'This is the list of our students\n';
+  let data;
+  try {
+    data = await countStudents(database);
+    res.send(`${pre}${data.join('\n')}`);
+  } catch (err) {
+    res.send(err.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Running on http://127.0.0.1:${port}`);
+});
+
+module.exports = app;
