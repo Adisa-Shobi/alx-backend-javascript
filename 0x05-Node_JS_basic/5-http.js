dfe9902d79b4ad443c6580a6737d36ec8915d7e8
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-21 15:00:56 +0000

    Update: '5-http.js' modified

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,5 +1,4 @@
 const http = require('http');
-const url = require('url');
 const countStudents = require('./3-read_file_async');
 
 const hostname = '127.0.0.1';
@@ -8,12 +7,10 @@ const port = 1245;
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  const reqUrl = url.parse(req.url).pathname;
-  if (reqUrl === '/') {
-    res.write('Hello Holberton School!');
-    res.end();
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
   }
-  if (reqUrl === '/students') {
+  if (req.url === '/students') {
     const database = process.argv[2];
     let data;
     try {
@@ -22,8 +19,7 @@ const app = http.createServer(async (req, res) => {
       res.end(err.message);
     }
     res.write('This is the list of our students\n');
-    res.write(data.join('\n'));
-    res.end();
+    res.end(data.join('\n'));
   }
 });
 
