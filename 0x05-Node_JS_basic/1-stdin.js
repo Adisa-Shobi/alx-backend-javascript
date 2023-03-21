b190560d1ec6d7037fed45bc0f3fdf849f706e59
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-21 07:29:46 +0000

    Update: '1-stdin.js' eslint adherent

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('readable', function () {
+process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
     process.stdout.write(`Your name is: ${chunk}`);
