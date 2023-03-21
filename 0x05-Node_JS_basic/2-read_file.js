0bcbb8861d47f787ed792741a8b0dad31c7995ef
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-21 07:35:49 +0000

    Update: '''2-read_file.js' bug fixed

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -13,7 +13,7 @@ function countStudents(path) {
   const numberOfStudents = lines.length ? lines.length - 1 : 0;
   console.log(`Number of students: ${numberOfStudents}`);
   const fields = {};
-  for (let i = 1; i < lines.length; i = 1 + 1) {
+  for (let i = 1; i < lines.length; i += 1) {
     const line = lines[i];
     const field = line[line.length - 1];
     if (field in fields) { fields[field].push(line[0]); } else { fields[field] = [line[0]]; }
