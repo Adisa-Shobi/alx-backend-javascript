4a8a1a2008a3ff91252452d0b52cf0999d0a6b06
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-26 19:44:58 +0000

    Create: '2-calcul_chai.js, 2-calcul_chai.test.js' migrated from assert to chai

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,77 @@
+const { expect } = require('chai');
+const calculateNumber = require('./1-calcul');
+
+describe('tests for the upgraded calculateNumber function', () => {
+  describe('test for SUM operation', () => {
+    it('should equal 5', () => {
+      expect(calculateNumber('SUM', 2, 3)).to.equal(5);
+    });
+  });
+
+  describe('test for SUM operation with two floats', () => {
+    it('should equal 6', () => {
+      expect(calculateNumber('SUM', 2.6, 3.2)).to.equal(6);
+    });
+  });
+
+  describe('test for SUM operation with first val floats', () => {
+    it('should equal 6', () => {
+      expect(calculateNumber('SUM', 2.6, 3)).to.equal(6);
+    });
+  });
+
+  describe('test for SUM operation with second val floats', () => {
+    it('should equal 5', () => {
+      expect(calculateNumber('SUM', 2, 3.2)).to.equal(5);
+    });
+  });
+
+  describe('test for SUBTRACT operation', () => {
+    it('should equal 5', () => {
+      expect(calculateNumber('SUBTRACT', 8, 3)).to.equal(5);
+    });
+  });
+
+  describe('test for SUBTRACT operation with two floats one bordeline',
+    () => {
+      it('should equal 0', () => {
+        expect(calculateNumber('SUBTRACT', 2.5, 3.2)).to.equal(0);
+      });
+    });
+
+  describe('test for SUBTRACT operation first val float', () => {
+    it('should equal 5', () => {
+      expect(calculateNumber('SUBTRACT', 8.3, 3)).to.equal(5);
+    });
+  });
+
+  describe('test for SUBTRACT operation second val float', () => {
+    it('should equal 4', () => {
+      expect(calculateNumber('SUBTRACT', 8, 3.5)).to.equal(4);
+    });
+  });
+
+  describe('test for SUBTRACT operation with -ve result', () => {
+    it('should equal 4', () => {
+      expect(calculateNumber('SUBTRACT', 2, 3.5)).to.equal(-2);
+    });
+  });
+
+  describe('test for DIVIDE operation with seccond value as 0', () => {
+    it('should equal Error', () => {
+      expect(calculateNumber('DIVIDE', 8, 0)).to.equal('Error');
+    });
+  });
+
+  describe('test for DIVIDE operation', () => {
+    it('should equal 5', () => {
+      expect(calculateNumber('DIVIDE', 10, 2)).to.equal(5);
+    });
+  });
+
+  describe('test for DIVIDE operation with two floats', () => {
+    it('should equal 4', () => {
+      expect(calculateNumber('DIVIDE', 12.3, 2.5)).to.equal(4);
+    });
+  });
+});
