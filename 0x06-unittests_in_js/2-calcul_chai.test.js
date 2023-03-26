e4a6c4c7cae99935d6c0398b366d9bd86e97b1f7
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-26 19:51:56 +0000

    Update: '-calcul_chai.js, 2-calcul_chai.test.js' modified

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,5 +1,5 @@
 const chai = require('chai');
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require('./2-calcul');
 
 describe('tests for the upgraded calculateNumber function', () => {
   describe('test for SUM operation', () => {
