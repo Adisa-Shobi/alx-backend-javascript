4cdc1880f78bf953697f2ce7ae170ae4c86c6cc4
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-27 00:27:45 +0000

    Update: '6-payment_token.js, 6-payment_token.test.js' modified

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -4,7 +4,7 @@ const getPaymentTokenFromAPI = require('./6-payment_token');
 describe('getPaymentTokenFromAPI', () => {
   it('should return if true is passed in', (done) => {
     getPaymentTokenFromAPI(true).then((res) => {
-      expect(res).to.include({ data: 'Successful response from the API' });
+      chai.expect(res).to.include({ data: 'Successful response from the API' });
       done();
     }).catch((err) => done(err));
   });
