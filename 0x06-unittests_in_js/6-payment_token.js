65eb29ec458a01656a8be97f2fd06207b629e752
Adisa-Shobi <s.oadisa.dev@gmail.com>
2023-03-27 00:19:33 +0000

    Update: '6-payment_token.js, 6-payment_token.test.js' debugged

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,7 +1,6 @@
-function getPaymentTokenFromAPI (success) {
-    return new Promise((resolve, reject) => {
-	if (success)
-	    resolve({data: 'Successful response from the API' });
-    });
+function getPaymentTokenFromAPI(success) {
+  if (success) {
+    return new Promise.resolve({ data: 'Successful response from the API' });
+  }
 }
 module.exports = getPaymentTokenFromAPI;
